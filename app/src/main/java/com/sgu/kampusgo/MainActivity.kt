7b23f2f48fcbfe79995dfd1953894d6ee656241e
ghjknehbor <troydevdan@gmail.com>
2026-09-15 09:52:52 +0700

    Lab 01 Hello KampusGo

--- app/src/main/java/com/sgu/kampusgo/MainActivity.kt
+++ app/src/main/java/com/sgu/kampusgo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             KampusGoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Troy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
